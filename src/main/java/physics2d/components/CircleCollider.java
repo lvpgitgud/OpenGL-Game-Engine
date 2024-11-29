284425ce4cf8f87ed5644a37ac058e0850e9e894
LVP <vienphatle@gmail.com>
2024-11-29 22:48:37 +0700

    Adding physics and events

---
+++ src/main/java/physics2d/components/CircleCollider.java
@@ -0,0 +1,13 @@
+package physics2d.components;
+
+import Components.Component;
+
+public class CircleCollider extends Component {
+    private float radius = 1f;
+    public float getRadius() {
+        return radius;
+    }
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+}
